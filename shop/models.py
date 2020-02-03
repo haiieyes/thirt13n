17145b5fa798cfae2ebb43285df51010dff2109b
root <root@ip-172-31-89-34.ec2.internal>
2020-02-03 10:21:32 +0000

    Fixing Images

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,8 @@ import  PIL
 from PIL import Image
 from django.db import models
 
+from pyuploadcare.dj.models import ImageField
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, blank=False)
@@ -9,10 +11,14 @@ class Product(models.Model):
     description = models.TextField(blank=False)
     cost = models.IntegerField(blank=False)
     quantity = models.IntegerField(blank=False, default=0)
-    image = models.ImageField(blank=True, null=True)
+    image = models.TextField(blank=False)
     
     def __str__(self):
         return self.name + " : " + self.sku
         
     def getCostInDollars(self):
         return self.cost/100
+        
+class Photo(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    photo = ImageField(null=True)
